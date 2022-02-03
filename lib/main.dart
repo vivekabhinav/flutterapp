77dcb445d5a2c650a7b5eaeea4a5f533ed4b8590
vivek <vivekabhinav7@gmail.com>
2022-02-03 12:26:56 +0530

    BODY , FAB

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('app bar'),
-          ) ,
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('app bar'),
+          centerTitle: true,
         ),
-      ));
+        body: Center(child: Text('hello world')),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {  },
+          child: Text('click'),
+        ),
+      ),
+    ),
+  );
 }
